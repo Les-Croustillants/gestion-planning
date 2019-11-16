58e02758ddd7d1b3bf85ed0e1e75c71094ebf9a8
Adrien <adrien.alary@limayrac.fr>
2019-11-16 12:04:48 +0100

    Push main controller

---
+++ src/Controller/mainController.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace app\mainController;
+
+
+use phpDocumentor\Reflection\Types\Boolean;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class mainController extends AbstractController
+{
+    public function home()
+    {
+
+    }
+
+    public function inscription()
+    {
+
+    }
+
+    public function connexion()
+    {
+
+    }
+
+    public function deconnexion()
+    {
+
+    }
+
+    public function showListeIntervenants()
+    {
+
+    }
+
+    public function showListeMatieres()
+    {
+
+    }
+
+    public function showListeJoursFeries()
+    {
+
+    }
+
+    public function assignerIntervenant(Boolean $matin, Intervenant $intervenant)
+    {
+
+    }
+
+    public function showCalendAnnee()
+    {
+
+    }
+
+    public function showCalendSemaine(Date $date)
+    {
+
+    }
+
+    public function ajouterJoursAbsences()
+    {
+
+    }
+
+    public function proposerEchange(Boolean $echangeIntraSemaine)
+    {
+
+    }
+
+    public function validerEchange()
+    {
+
+    }
+}
