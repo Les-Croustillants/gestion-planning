6a8cb1eaf986769a6e9693139a0da19a4d701814
Adrien <adrien.alary@limayrac.fr>
2019-11-25 09:04:23 +0100

    Correction controller

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -9,6 +9,7 @@ use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Routing\Annotation\Route;
 
 class mainController extends AbstractController
 {
